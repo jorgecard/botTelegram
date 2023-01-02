ed9b2801e4ee24b9e33ed3bc7dfd8122b38cad6e
jorgecard <121685867+jorgecard@users.noreply.github.com>
2023-01-01 21:42:50 -0500

    Update jorgebot.py

--- jorgebot.py
+++ jorgebot.py
@@ -65,8 +65,6 @@ def por_vencer(update: Update, context: CallbackContext) -> None:
 def vencidos(update: Update, context: CallbackContext) -> None:
     mensaje_con_clientes, df = iptv.vencidos()
     update.message.reply_text(mensaje_con_clientes)
-    df.to_json()
-    update.message.reply_text(df, parse_mode='Markdown')
     update.message.reply_text(tornar)
 
 
